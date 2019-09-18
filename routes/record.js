76fba2a41657e02e548de3d1a9c25d1a72dbc277
Raiden <rodes5292@gmail.com>
2019-09-18 10:54:09 +0800

    新增支出頁面功能

--- routes/record.js
+++ routes/record.js
@@ -6,30 +6,13 @@ router.get("/", (req, res) => {
   res.send("列出所有Record");
 });
 
-// router.get("/new", (req, res) => {
-//   console.log("I am in GET /new");
-//   return res.render("new");
-// });
-
-// router.post("/", (req, res) => {
-//   const record = Record({
-//     name: req.body.name
-//     // 儲存 userId
-//     // userId: req.user._id
-//   });
-//   record.save(err => {
-//     if (err) return console.error(err);
-//     return res.redirect("/");
-//   });
-// });
-
 // 列出全部 Record
 router.get("/", (req, res) => {
   res.send("列出所有 Record");
 });
 // 新增一筆 Record 頁面
 router.get("/new", (req, res) => {
-  res.send("新增 Record 頁面");
+  return res.render("new");
 });
 // 顯示一筆 Record 的詳細內容
 router.get("/:id", (req, res) => {
@@ -37,7 +20,16 @@ router.get("/:id", (req, res) => {
 });
 // 新增一筆  Record
 router.post("/", (req, res) => {
-  res.send("建立 Record");
+  const record = new Record({
+    name: req.body.name,
+    category: req.body.category,
+    date: req.body.date,
+    amount: req.body.amount
+  });
+  record.save(err => {
+    if (err) return console.error(err);
+    return res.redirect("/");
+  });
 });
 // 修改 Record 頁面
 router.get("/:id/edit", (req, res) => {
