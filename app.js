a50808cac3077a1b956bd1d1db4399a9bd2aff4e
ZhaoHouLin <rodes5292@gmail.com>
2019-09-19 19:55:59 +0800

    Add connect-flash for UI messages

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const bodyParser = require("body-parser");
 const methodOverride = require("method-override");
 const session = require("express-session");
 const passport = require("passport");
+const flash = require("connect-flash")
 
 if (process.env.NODE_ENV !== 'production') {      // 如果不是 production 模式
   require('dotenv').config()                      // 使用 dotenv 讀取 .env 檔案
@@ -36,6 +37,10 @@ db.once("open", () => {
   console.log("mongodb connected!");
 });
 
+
+
+
+
 app.use(
   session({
     secret: "your secret key",
@@ -55,10 +60,24 @@ app.use((req, res, next) => {
   next();
 });
 
+
+app.use(flash())
+// 建立 local variables
+app.use((req, res, next) => {
+  res.locals.user = req.user
+  res.locals.isAuthenticated = req.isAuthenticated()    // 辨識使用者是否已經登入的變數，讓 view 可以使用
+
+  // 新增兩個 flash message 變數 
+  res.locals.success_msg = req.flash('success_msg')
+  res.locals.warning_msg = req.flash('warning_msg')
+  next()
+})
+
 app.use("/", require("./routes/home"));
 app.use("/records", require("./routes/record"));
 app.use("/users", require("./routes/user"));
 app.use("/auth", require("./routes/auths"))
+
 
 app.listen(port, () => {
   console.log(`http://localhost:${port}`);
